d7982068e0e8ce06159625053e2683db15337091
hammam <h.besaiso95@gmail.com>
2020-03-26 15:49:57 +0200

    whatsapp

--- src/mama.js
+++ src/mama.js
@@ -1,18 +1,22 @@
 import React from "react"
 import text from "./images/text.gif"
+import logo from "./images/logo.jpg"
+
 
 function mama() {
     return (
         <div className="container-fluid mt-10">
             <div className="card bg-graan text-black"
-                style={{ width: "25rem", height: "15rem", }}
             >
-                <img className="card-img" src={text} alt="Card image"
-
-                />
+                <img className="card-img " src={logo} alt="Card image" />
                 <div className="card-img-overlay">
                     <h5 className="card-title">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h5>
-                    <p className="card-text">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+                    <p className="card-text">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+                     <img className="img rounded-circle" src={text} alt="Card image"
+                            style={{ width: "2rem", height: "2rem", marginLeft: "5px" }}
+
+                        />
+                    </p>
                 </div>
             </div>
         </div>
